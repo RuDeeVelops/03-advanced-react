ce0e5dbdca431ef0c3a5082c8ba20a23aea27537
Rodolfo Fanti <fanti.rodolfo@gmail.com>
2024-10-22 21:39:04 +0200

    updated exercises

--- src/tutorial/01-useState/starter/04-useState-object.jsx
+++ src/tutorial/01-useState/starter/04-useState-object.jsx
@@ -1,16 +1,25 @@
 import { useState } from 'react'
 
 const UseStateObject = () => {
-  const [name, setName] = useState('Mark')
-  const [age, setAge] = useState(33)
-  const [hobby, setHobby] = useState('darts')
+  const [person, setPerson] = useState({
+    name: 'Mark',
+    age: 33,
+    hobby: 'darts',
+  })
+
+  const switchPerson = () => {
+    setPerson({ name: 'John', age: 45, hobby: 'Moon Gliding' })
+  }
 
   return (
     <div>
       <h1>useState object example</h1>
-      <h2>{name}</h2>
-      <h2>{age}</h2>
-      <h2>{hobby}</h2>
+      <h2>{person.name}</h2>
+      <h2>{person.age}</h2>
+      <h2>{person.hobby}</h2>
+      <button type="button" className="btn" onClick={switchPerson}>
+        Switch Person
+      </button>
     </div>
   )
 }
--- src/tutorial/01-useState/starter/05-useState-gotcha.jsx
+++ src/tutorial/01-useState/starter/05-useState-gotcha.jsx
@@ -1,5 +1,27 @@
-const UseStateGotcha = () => {
-  return <h2>useState "gotcha"</h2>;
-};
+import { useState } from 'react'
 
-export default UseStateGotcha;
+const UseStateGotcha = () => {
+  const [counter, setCounter] = useState(0)
+  const [counterAlt, setCounterAlt] = useState(0)
+
+  const increaseCount = () => {
+    setCounter((prevValue) => {
+      const newState = prevValue + 1
+      console.log(`prev:${prevValue}`)
+      console.log(`Current:${newState}`)
+      return newState
+    })
+  }
+
+  return (
+    <div>
+      <h2>useState "gotcha"</h2>
+      <p>{counter}</p>
+      <button className="btn" type="button" onClick={increaseCount}>
+        Increase
+      </button>
+    </div>
+  )
+}
+
+export default UseStateGotcha
